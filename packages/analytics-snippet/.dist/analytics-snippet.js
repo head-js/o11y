c69e9fe345b8de720c228eeeef4ac990824670d6
hbrls <shuaizhexu@gmail.com>
2022-10-24 10:36:16 +0800

    A: analytic-snippet

---
+++ packages/analytics-snippet/.dist/analytics-snippet.js
@@ -0,0 +1,68 @@
+(function () {
+  'use strict';
+
+  var ANALYTICS_WRITE_KEY = '';
+  var ANALYTICS_LEGACY_SETTINGS = {};
+  var analytics = window.analytics = window.analytics || [];
+  if (!analytics.initialize) if (analytics.invoked) window.console && console.error && console.error('Segment snippet included twice.');else {
+    analytics.invoked = !0;
+    analytics.methods = [
+    // 'screen',
+    // 'register',
+    // 'deregister',
+    // 'trackSubmit',
+    // 'trackClick',
+    // 'trackLink',
+    // 'trackForm',
+    // 'pageview',
+    'identify',
+    // 'reset',
+    // 'group',
+    'track', 'ready',
+    // 'alias',
+    // 'debug',
+    'page'
+    // 'once',
+    // 'off',
+    // 'on',
+    // 'addSourceMiddleware',
+    // 'addIntegrationMiddleware',
+    // 'setAnonymousId',
+    // 'addDestinationMiddleware',
+    ];
+
+    analytics.factory = function (e) {
+      return function () {
+        var t = Array.prototype.slice.call(arguments);
+        t.unshift(e);
+        analytics.push(t);
+        return analytics;
+      };
+    };
+    for (var e = 0; e < analytics.methods.length; e++) {
+      var key = analytics.methods[e];
+      analytics[key] = analytics.factory(key);
+    }
+    analytics.load = function (settings, options) {
+      var t = document.createElement('script');
+      t.type = 'text/javascript';
+      t.async = !0;
+      t.src = '';
+      var n = document.getElementsByTagName('script')[0];
+      n.parentNode.insertBefore(t, n);
+    };
+    analytics.SNIPPET_VERSION = '4.13.1';
+    var ANALYTICS_LOAD_SETTINGS = {
+      cdnSettings: ANALYTICS_LEGACY_SETTINGS,
+      writeKey: ANALYTICS_WRITE_KEY,
+      cdnURL: 'https://cdn.segment.com'
+    };
+    var ANALYTICS_LOAD_OPTIONS = {};
+    analytics._writeKey = ANALYTICS_WRITE_KEY;
+    analytics._loadSettings = ANALYTICS_LOAD_SETTINGS;
+    analytics._loadOptions = ANALYTICS_LOAD_OPTIONS;
+    analytics.load(ANALYTICS_LOAD_SETTINGS, ANALYTICS_LOAD_OPTIONS);
+    analytics.page();
+  }
+
+})();
---
+++ packages/analytics-snippet/rollup.config.js
@@ -0,0 +1,41 @@
+import eslint from '@rollup/plugin-eslint';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import json from '@rollup/plugin-json';
+
+
+export default [
+  {
+    input: 'src/index.js',
+
+    external: [
+      'core-js/modules/es.array.slice.js',
+      'core-js/modules/es.object.to-string.js',
+      'core-js/modules/es.promise.js',
+    ],
+
+    plugins: [
+      eslint(),
+
+      json(),
+
+      commonjs({
+        sourceMap: false,
+      }),
+
+      resolve({
+        browser: true,
+      }),
+
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+      }),
+    ],
+
+    output: [
+      { file: '.dist/analytics-snippet.js', format: 'iife', name: 'AnalyticsSnippet' },
+    ],
+  },
+];
